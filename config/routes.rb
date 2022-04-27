8c3b39d47c3170e2b819c23008411b836cbc027b
Ashley Lampard <ashleylampard@live.com>
2022-04-27 06:35:12 +0100

    moved new review into the restaurant how view and add scss styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
     resources :restaurants, only: [ :index, :create, :new, :show ] do
-      resources :reviews, only: [ :new, :create ]
+      resources :reviews, only: [ :create ]
     end
   end
 end
